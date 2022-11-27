d144a6cb31784f40558cfe1fa3f2d8fff415a37c
Your Name <you@example.com>
2022-11-27 16:26:55 +0700

    hhh

---
+++ app/src/main/java/com/example/sunhappy/functions/viewcart/ViewDetailCartActivity.java
@@ -0,0 +1,17 @@
+package com.example.sunhappy.functions.viewcart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sunhappy.R;
+
+public class ViewDetailCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_detail_cart);
+    }
+
+}
