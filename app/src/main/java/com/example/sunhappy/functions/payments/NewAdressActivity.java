41f31b461d7f104395faebcaeb3ee6df72649601
PongPongwIthloVe <116481040+PongPongwIthloVe@users.noreply.github.com>
2022-11-28 13:41:40 +0700

    update Setting, Setting Account and add NewAdress Activity

--- app/src/main/java/com/example/sunhappy/functions/payments/NewAdressActivity.java
+++ app/src/main/java/com/example/sunhappy/functions/payments/NewAdressActivity.java
@@ -1,16 +1,28 @@
 package com.example.sunhappy.functions.payments;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
 
 import com.example.sunhappy.R;
+import com.example.sunhappy.databinding.ActivityNewAdressBinding;
 
 public class NewAdressActivity extends AppCompatActivity {
-
+    ActivityNewAdressBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_adress);
+        binding = ActivityNewAdressBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ActionBar actionBar = getSupportActionBar();
+        //actionBar.setTitle("Thiết lập tài khoản");
+        actionBar.setTitle(Html.fromHtml("<font color='#ffd24c'>Địa chỉ mới</font>"));
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_yellow);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/example/sunhappy/functions/setting/SettingAccountActivity.java
+++ app/src/main/java/com/example/sunhappy/functions/setting/SettingAccountActivity.java
@@ -1,16 +1,30 @@
 package com.example.sunhappy.functions.setting;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
 
 import com.example.sunhappy.R;
+import com.example.sunhappy.databinding.ActivityNewAdressBinding;
+import com.example.sunhappy.databinding.ActivitySettingAccountBinding;
 
 public class SettingAccountActivity extends AppCompatActivity {
-
+    ActivitySettingAccountBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting_account);
+        //setContentView(R.layout.activity_setting_account);
+        binding = ActivitySettingAccountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ActionBar actionBar = getSupportActionBar();
+        //actionBar.setTitle("Thiết lập tài khoản");
+        actionBar.setTitle(Html.fromHtml("<font color='#ffd24c'>Sửa hồ sơ</font>"));
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_yellow);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
