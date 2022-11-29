7f8407b8374857fc305b3103a90178c8642130ee
anhttl20411 <anhttl20411@st.uel.edu.vn>
2022-11-29 11:36:56 +0700

    commit sau khi thi gk

---
+++ app/src/main/java/com/example/sunhappy/functions/viewnotification/NotificationActivity.java
@@ -0,0 +1,16 @@
+package com.example.sunhappy.functions.viewnotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sunhappy.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
