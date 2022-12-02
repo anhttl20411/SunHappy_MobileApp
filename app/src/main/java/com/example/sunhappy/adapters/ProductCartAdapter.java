f9fea96dccb605f735a65a8d5c27a41aefcfb7ba
Your Name <you@example.com>
2022-12-03 01:08:15 +0700

    updatee

--- app/src/main/java/com/example/sunhappy/adapters/ProductCartAdapter.java
+++ app/src/main/java/com/example/sunhappy/adapters/ProductCartAdapter.java
@@ -8,12 +8,17 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sunhappy.R;
+import com.example.sunhappy.databinding.DialogUpdateSizeColorBinding;
+import com.example.sunhappy.functions.manageorder.ViewOderPreparingActivity;
 import com.example.sunhappy.functions.viewcart.ProductCartActivity;
 import com.example.sunhappy.models.ProductCart;
 
@@ -58,6 +63,10 @@ public class ProductCartAdapter extends BaseAdapter {
             holder.txtDescription = view.findViewById(R.id.txt_DescriptionProduct);
             holder.txtPrice = view.findViewById(R.id.txt_Price);
             holder.imvExpand = view.findViewById(R.id.imv_Expand);
+//            holder.chkSelect = view.findViewById(R.id.chk_Select);
+//            holder.imvMinus = view.findViewById(R.id.imv_Minus);
+//            holder.imvPlus = view.findViewById(R.id.imv_Plus);
+//            holder.txtValues = view.findViewById(R.id.txt_Values);
 
             view.setTag(holder);
         }else {
@@ -69,17 +78,44 @@ public class ProductCartAdapter extends BaseAdapter {
         holder.txtName.setText(p.getProductName());
         holder.txtDescription.setText(p.getProductDescription());
         holder.txtPrice.setText(String.valueOf(p.getProductPrice()));
+// điều kiện ẩn hiện button
+//        int sl;
+//        ViewHolder viewHolder = new ViewHolder();
+//        sl = Integer.parseInt(viewHolder.txtValues.getText().toString());
+//        if (sl >10){
+//            viewHolder.imvPlus.setVisibility(View.INVISIBLE);
+//            ViewHolder.imvMinus.setVisibility(View.VISIBLE);
+//        }else if(sl<=1){
+//            ViewHolder.imvMinus.setVisibility(View.INVISIBLE);
+//        }else if (sl>=1){
+//            viewHolder.imvPlus.setVisibility(View.INVISIBLE);
+//            viewHolder.imvMinus.setVisibility(View.INVISIBLE);
+//        }
+//        viewHolder.imvPlus.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                int update= Integer.parseInt(viewHolder.txtValues.getText().toString() + 1);
+//            }
+//        });
+
         holder.imvExpand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                activity.openDialogChooseColor(p);
+                activity.openDialogUpdateColor(p);
             }
         });
 
         return view;
     }
     public static class ViewHolder{
-        ImageView imvThumb, imvExpand ;
-        TextView txtName,txtDescription, txtPrice;
+        public View chkSelect;
+        public View imvDelete;
+        public AdapterView<Adapter> txtDescriptionProduct;
+
+        ImageView imvThumb;
+        ImageView imvExpand;
+        static ImageView imvMinus;
+        ImageView imvPlus ;
+        TextView txtName,txtDescription,txtValues,  txtPrice;
     }
 }
--- app/src/main/java/com/example/sunhappy/functions/viewcart/ProductCartActivity.java
+++ app/src/main/java/com/example/sunhappy/functions/viewcart/ProductCartActivity.java
@@ -3,11 +3,17 @@ package com.example.sunhappy.functions.viewcart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 import com.example.sunhappy.R;
 import com.example.sunhappy.adapters.ProductCartAdapter;
 import com.example.sunhappy.databinding.ActivityProductCartBinding;
+import com.example.sunhappy.functions.manageorder.ViewOderPreparingActivity;
+import com.example.sunhappy.functions.payments.PaymentActivity;
 import com.example.sunhappy.models.ProductCart;
 
 import java.util.ArrayList;
@@ -17,6 +23,7 @@ public class ProductCartActivity extends AppCompatActivity {
     ActivityProductCartBinding binding;
     ProductCartAdapter adapter;
     ArrayList<ProductCart> productCartArrayList;
+    private ProductCart p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +32,49 @@ public class ProductCartActivity extends AppCompatActivity {
         binding = ActivityProductCartBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         loadData();
+
+        binding.btnBuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProductCartActivity.this, PaymentActivity.class);
+                startActivity(intent);
+            }
+        });
+//        binding.chkSelectAll.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new
+//
+//            }
+//        });
     }
 
-    public void openDialogChooseColor(ProductCart p) {
+
+    public void openDialogUpdateColor(ProductCart p) {
         Dialog dialog = new Dialog(ProductCartActivity.this);
-        dialog.setContentView(R.layout.dialog_choose_size_color);
+        dialog.setContentView(R.layout.dialog_update_size_color);
+        dialog.show();
+
+       Button btn_UpdateToCartConfirm;
+       btn_UpdateToCartConfirm= dialog.findViewById(R.id.btn_UpdateToCartConfirm);
+       btn_UpdateToCartConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // chèn sự kiện chọn cái radio cho nó cập nhật qua bên kia
+                Intent intent = new Intent(ProductCartActivity.this, PaymentActivity.class);
+                startActivity(intent);
+                dialog.dismiss();
+            }
+        });
+        View imvClose = dialog.findViewById(R.id.imv_Close);
+        imvClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
         dialog.show();
     }
-
     private void loadData(){
         productCartArrayList = new ArrayList<>();
         productCartArrayList.add(new ProductCart(R.drawable.imv_white_polo, "Áo polo nam", "Aó polo màu đen",180000));
@@ -42,4 +84,13 @@ public class ProductCartActivity extends AppCompatActivity {
         binding.lvProduct.setAdapter(adapter);
     }
 
+//    public void openDialogDelete(ProductCart p) {
+//        View imvDelete = findViewById(R.id.imv_Delete);
+//        imvDelete.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+//    }
 }
